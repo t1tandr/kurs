bf466548b99833f8f21cdf964f815de7cc5fb001
Nick <75449627+t1tandr@users.noreply.github.com>
2021-03-06 14:15:19 +0200

    lesson9

---
+++ lesson9/script/script.js
@@ -0,0 +1,289 @@
+// let userName = "Denis"
+// let age = 27
+// let isSmoking = false
+// console.log("Hello World")
+// console.log(userName, age, isSmoking)
+// // alert("Hello " + userName)
+// let myage = prompt("How old are you?")  
+// alert("I'm "+ myage + " years old")
+// alert(`I'm ${myage} years old`)
+// console.log(myage + " is " + typeof(myage))
+// myage = Number(myage)
+// console.log(myage + " is " + typeof(myage))
+// // let sure = confirm("Are you sure?")
+// // alert(sure)
+// let y = 27=="27"
+// let z = 27==="27"
+// console.log(y, z)
+// let destemp = 3
+// let temp = 4
+// let mood = false
+// if (temp > destemp && mood==true){
+//     alert("Go out")
+// }
+// else if (temp==destemp){
+//     alert("GG")
+// }
+// else{
+//     alert("Stay home!")
+// }
+// alert('Введите разные значения для обеих функций!')
+// let x = prompt("Введите первое значение!")
+// let y = prompt("Введите второе значение!")
+// console.log(isNaN(x), isNaN(y))
+
+    // 1 ЗАДАЧА
+
+// let x = Number(prompt('Введите чилсо!'))
+// let y = 1
+// if(isNaN(x)==true || x<0){
+//     alert("Введите занчение еще раз")
+// }
+// else{
+//     alert(x+y)
+// }
+
+        // 2 ЗАДАЧА
+// let x = Number(prompt('Введите чилсо!'))
+// if(x>0){
+//     alert(x+1)
+// }
+// else{
+//     alert(x-2)
+// }
+
+        // 3 zadacha
+// let x = Number(prompt('Введите чилсо!'))
+// if(x>0){
+//     alert(x+1)
+// }
+// else if(x==0){
+//     let x = 10
+//     alert(x)
+// }
+// else{
+//     alert(x-2)
+// }
+
+        // 4zadacha
+// let x = Number(prompt('Введите 1 чилсо!'))
+// let y = Number(prompt('Введите 2 чилсо!'))
+// let z = Number(prompt('Введите 3 чилсо!'))
+// if(x>0 && y>0 && z>0){
+//     alert("3")
+// }
+// else if(x<0 && y>0 && z>0){
+//     alert("2")
+// }
+// else if(x>0 && y<0 && z>0){
+//     alert("2")
+// }
+// else if(x>0 && y>0 && z<0){
+//     alert("2")
+// }
+// else if(x<0 && y<0 && z>0){
+//     alert("1")
+// }
+// else if(x<0 && y>0 && z<0){
+//     alert("1")
+// }
+// else if(x>0 && y<0 && z<0){
+//     alert("1")
+// }
+// else{
+//     alert("Ни одного значения нет положительного!")
+// }
+
+        // 5zadacha\
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // let z = Number(prompt('Введите 3 чилсо!'))
+        // if(x>0 && y>0 && z>0){
+        //     alert("3 положительных")
+        //     alert("0 отрицательных")
+        // }
+        // else if(x<0 && y>0 && z>0){
+        //     alert("2 положительных")
+        //     alert("1 отрицательная")
+        // }
+        // else if(x>0 && y<0 && z>0){
+        //     alert("2 положительных")
+        //     alert("1 отрицательная")
+        // }
+        // else if(x>0 && y>0 && z<0){
+        //     alert("2 положительных")
+        //     alert("1 отрицательная")
+        // }
+        // else if(x<0 && y<0 && z>0){
+        //     alert("1 положительное")
+        //     alert("2 отрицательных")
+        // }
+        // else if(x<0 && y>0 && z<0){
+        //     alert("1 положительное")
+        //     alert("2 отрицательных")
+        // }
+        // else if(x>0 && y<0 && z<0){
+        //     alert("1 положительное")
+        //     alert("2 отрицательных")
+        // }
+        // else{
+        //     alert("Ни одного значения нет положительного! Но зато все отрицательные")
+        // }
+
+
+
+        // 6zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // if(x>y){
+        //     alert(x)
+        // }
+        // else{
+        //     alert(y)
+        // }
+
+
+        // 7zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // if(x>y){
+        //     alert(1 bolshe)
+        // }
+        // else{
+        //     alert(2 bolshe)
+        // }
+
+
+        // 8zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // if(x>y){
+        //     alert(x)
+        //     alert(y)
+        // }
+        // else{
+        //     alert(y)
+        //     alert(x)
+        // }
+
+
+        // 9zadacha
+        // let x = Number(prompt('Введите A чилсо!'))
+        // let y = Number(prompt('Введите B чилсо!'))
+        // if(x>y){
+        //     alert("znacheniye B = " + x)
+        //     alert("znacheniye A = " + y)
+        // }
+        // else{
+        //     alert("B = " + y)
+        //     alert("A = " + x)
+        // }
+        
+
+        // 10zadacha
+        // let a = Number(prompt('Введите A чилсо!'))
+        // let b = Number(prompt('Введите B чилсо!'))
+        // if(a==b){
+        //     let a = 0
+        //     let b = 0
+        //     alert('A = ' + a + 'B = ' + b)
+        // }
+        // else{
+        //     let a = a+b
+        //     let b = a+b
+        //     alert('A = ' + a + 'B = ' + b)
+        // }
+
+
+        // 11 zadacha
+        // let a = Number(prompt('Введите A чилсо!'))
+        // let b = Number(prompt('Введите B чилсо!'))
+        // if(a==b){
+        //     let a = 0
+        //     let b = 0
+        //     alert('A = ' + a + 'B = ' + b)
+        // }
+        // else if(a>b){
+        //     let a = a
+        //     let b = a
+        //     alert('A = ' + a + 'B = ' + b)
+        // }
+        // else if(a<b){
+        //     let a = b
+        //     let b = b
+        //     alert('A = ' + a + 'B = ' + b)
+        // }
+        // else{
+
+        // }
+
+
+        // 12zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // let z = Number(prompt('Введите 3 чилсо!'))
+        // if(x<y && x<z){
+        //     alert(x)
+        // }
+        // else if(y<z && y<x){
+        //     alert(y)
+        // }
+        // else if(z<y && z<x){
+        //     alert(z)
+        // }
+        // else{
+
+        // }
+
+
+        // 13zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // let z = Number(prompt('Введите 3 чилсо!'))
+        // if(x<y<z && z<y<x){
+        //     alert(y)
+        // }
+        // else if(x<z<y && y<z<x){
+        //     alert(z)
+        // }
+        // else if(y<x<z && z<x<y){
+        //     alert(x)
+        // }
+        // else{
+
+        // }
+
+        // 14zadacha
+        // let x = Number(prompt('Введите 1 чилсо!'))
+        // let y = Number(prompt('Введите 2 чилсо!'))
+        // let z = Number(prompt('Введите 3 чилсо!'))
+        // if(x>y && x>z && y<x && y>z && z<x && z<y){
+        //     alert(x,y,z)
+        // }
+        // else if(x<y && x>z && y>x && y>z && z<x && z<y){
+        //     alert(y,x,z)
+        // }
+        // else if(x<y && x<z && y>x && y>z && z>x && z<y){
+        //     alert(y,z,x)
+        // }
+        // else if(x>y && x>z && y<x && y<z && z<x && z>y){
+        //     alert(x,z,y)
+        // }
+        // else if(x>y && x<z && y<x && y<z && z>x && z>y){
+        //     alert(x,z,y)
+        // }
+        // else if(x<y && x>z && y<x && y<z && z<x && z>y){
+        //     alert(x,y,z)
+        // }
+        // else{
+
+        // }
+
+
+        // 15zadacha
+        let x = Number(prompt('Введите 1 чилсо!'))
+        let y = Number(prompt('Введите 2 чилсо!'))
+        let z = Number(prompt('Введите 3 чилсо!'))
+     
+        console.log(Math.max(x, y, z)) 
+        console.log(Math.min(x, y, z))
